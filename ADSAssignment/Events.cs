29bf8f5e1c9b616ecb926d4add36523a3292bd94
ratihng <ngestrini@gmail.com>
2017-10-03 12:13:18 +0200

    Initial codes

---
+++ ADSAssignment/Events.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADSAssignment
+{
+    class Events
+    {
+        public Events()
+        {
+
+        }
+
+        public void TramArrival()
+        {
+
+        }
+
+        public void TramDeparture()
+        {
+
+        }
+
+        public void TramTurnAround()
+        {
+
+        }
+
+        public void TramDwellTime()
+        {
+
+        }
+
+        public void DoorsBlock()
+        {
+
+        }
+
+        public void ScheduleChange()
+        {
+
+        }
+    }
+}
---
+++ ADSAssignment/Stochastic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADSAssignment
+{
+    class Stochastic
+    {
+
+        public int GeneratePassengerArriving(int StationId, int TimeofArrival)
+        {
+
+            return 0;
+        }
+
+        public int GeneratePassengerLeaving(int StationId)
+        {
+
+            return 0;
+        }
+
+        public int GenerateDrivingTimes(int FromStationId, int ToStationId)
+        {
+
+            return 0;
+        }
+
+        public bool IsDoorsBlocked()
+        {
+
+            return true;
+        }
+    }
+}
